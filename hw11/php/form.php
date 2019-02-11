b05830133386874ddf0a014698321ae61a98811b
Ann Leleko <vredina.ann@gmail.com>
2019-02-11 20:08:59 +0200

    fix mistakes. All is ready

--- hw11/php/form.php
+++ hw11/php/form.php
@@ -8,30 +8,36 @@
 </head>
 <body>
 <?php
-$firstName = ($_POST['firstName']);
-$secondName = ($_POST['secondName']);
-$sex = ($_POST['sex']);
-$age = ($_POST['age']);
-$picture = 'not loaded';
-if(isset($_FILES['picture'])) $picture = 'loaded';
-//$birthday = date('Y-m-d', strtotime($_POST['birthday']));
-if(isset($_POST['email'])) $email = ($_POST['email']);
+if(!empty($_POST['firstName'])) $firstName = ($_POST['firstName']);
+else {
+  if ($sex='woman') $firstName = 'Jane';
+  else {
+    if ($sex='man') $firstName = 'John';
+    else $firstName = 'Anonymous';
+  }
+}
+if(!empty($_POST['secondName'])) $secondName = ($_POST['secondName']);
+else $secondName = 'Doe';
+if(!empty($_POST['sex'])) $sex = ($_POST['sex']);
+else $sex = 'sexless';
+if(!empty($_POST['age'])) $age = ($_POST['age']);
+else $age ='';
+if(!empty($_FILES['picture'])) $picture = 'loaded';
+else $picture = 'not loaded';
+if(!empty($_POST['birthday'])) $birthday = date('d.m.y', strtotime($_POST['birthday']));
+else $birthday =' one of these fine days';
+if(!empty($_POST['email'])) $email = ($_POST['email']);
 else $email = 'sorry, dad said do not give email to strangers';
-if(isset($_POST['phone'])) $phone = ($_POST['phone']);
-else $phone = 'sorry, mom said do not give phone number to strangers';
-
-if(isset($_POST['pass'])) $pass = ($_POST['pass']);
+if(!empty($_POST['phone'])) $phone = ($_POST['phone']);
+else $phone = '... oh! sorry, mom said do not give phone number to strangers';
+if(!empty($_POST['pass'])) $pass = ($_POST['pass']);
 else $pass = 'Oh! I forget enter it ... again';
-
-if(isset($_POST['url'])) $url= ($_POST['url']);
+if(!empty($_POST['url'])) $url= ($_POST['url']);
 else $url = "sorry, I do not do it yet ...";
-
-if(isset($_POST['hobbies'])) $hobbies = ($_POST['hobbies']);
+if(!empty($_POST['hobbies'])) $hobbies = ($_POST['hobbies']);
 else $hobbies = "... what am i talking about!? I don't like anything.";
-
-if(isset($_POST['comment'])) $comment = ($_POST['comment']);
+if(!empty($_POST['comment'])) $comment = ($_POST['comment']);
 else $comment = 'nothing. I do not like talking.';
-
 
 ?>
 <main class="row">
@@ -44,28 +50,41 @@ else $comment = 'nothing. I do not like talking.';
             <div class="card-body">
               <h1 class="text-center">Hi! My name is <?php echo $firstName.' '.$secondName?></h1>
               <p><?php echo 'I am a '.$sex.' '.$age.' years old. '.'My picture is '.$picture?></p>
-              <p><?php echo 'My B-day on '.' so you can write me on '.$email.' or call me and congratulate. My telephone number is '.$phone?></p>
+              <p><?php echo 'My B-day on '.$birthday.' so you can write me on '.$email.' or call me and congratulate. My telephone number is '.$phone?></p>
               <p><?php echo 'I always forget my password, so please remind me it ('.$pass.'). Also you can visit my web-site ('.$url.').'?></p>
-              <p><?php echo 'I like '.$hobbies.' and do not like '?></p>
-              <p>
-                  <?php
-                  $hate = $_POST['rr'];
-                  if(empty($hate)) {
-                    echo(' nothing, I love everything');
-                  }
-                  else {
-                    $N = count($hate);
-                    for($i=0; $i < $N; $i++)
-                    {
-                      echo($hate[$i]."  ");
+              <p><?php echo 'I like '.$hobbies.' and do not like '?>
+              <span>
+                    <?php
+                    if(!empty($_POST['rr'])) {
+                        $hate = $_POST['rr'];
+                        $N = count($hate);
+                        for($i=0; $i < $N; $i++) {
+                            echo($hate[$i].'  ');
+                        }
+                    } else {
+                        echo(' nothing, I love everything');
                     }
+                    ?>
+              </span>
+              </p>
+              <p><?php echo 'Finally, I want to say:'?>
+                <span><?php echo $comment?></span>
+              </p>
+              <p><?php echo 'And at the end, from the proposed I chose:'?>
+                <span>
+                  <?php
+                  if(!empty($_POST['version'])) {
+                      $version = $_POST['version'];
+                      $N = count($version);
+                      for($i=0; $i < $N; $i++) {
+                          echo($version[$i].'  ');
+                      }
+                  } else {
+                      echo(' nothing');
                   }
-                ?>
-                </p>
-              <p><?php echo 'Considering all of the above, I want to say:'?></p>
-              <p><?php echo $comment?></p>
-              <p><?php echo 'and at the end, from the proposed I chose:'?></p>
-              <p><?php foreach ($_POST['version'] as $keys=>$values) echo $values.' ';?></p>
+                  ?>
+              </span>
+              </p>
             </div>
         </div>
     </article>
@@ -82,27 +101,26 @@ else $comment = 'nothing. I do not like talking.';
             $userName = explode('@',$email);
             echo $userName[0];
             ?></p>
-
       </div>
     </div>
   </article>
 
   <article class="col-xs-12 col-sm-6 col-md-4">
     <div class="card m-1">
-      <div class="card-header bg-warning text-center">
+      <div class="card-header bg-info text-center">
         <h2 class="card-title">Завдання 3</h2>
         <p class="card-text">Написать генератор рандомних паролів, які складаються з чисел, малих і великих літер латинського алфавіту, і які повинні бути довші за 10 літер (включно) не використовуючи функцію rand()</p>
       </div>
       <div class="card-body">
         <p class="text-center">
             <?php
-            function genPass($length=10){
-              $string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-              $shufleString = str_shuffle($string);
-              $pass = substr($shufleString,$length);
-              return $pass;
-              }
-            echo $pass
+            function genPass(){
+                $string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                $shufleString = str_shuffle($string);
+                $newPass = substr($shufleString,0,10);
+                print $newPass;
+            }
+            genPass();
             ?></p>
       </div>
     </div>
@@ -110,10 +128,3 @@ else $comment = 'nothing. I do not like talking.';
 
 </body>
 </html>
-
-
-/*
-$length = [10,11,12,13,14,15,16,17,18,19,20];
-$newLength = shuffle($length);
-and used $newLength[0] as length pass
-*/
